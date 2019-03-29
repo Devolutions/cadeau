d967558e548feb8caa0a332d28c005b38dd37bc7
Marc-André Moreau <marcandre.moreau@gmail.com>
2019-03-29 16:04:44 -0400

    xpp: reduce macro usage in Halide generator

--- libxpp/halide/generator.cpp
+++ libxpp/halide/generator.cpp
@@ -4,25 +4,11 @@
 #define i16(x) (cast<int16_t>(x))
 #define u8(x) (cast<uint8_t>(x))
 
-#define rgbb(u, v) (i16(inputRgb(0, u, v)))
-#define rgbg(u, v) (i16(inputRgb(1, u, v)))
-#define rgbr(u, v) (i16(inputRgb(2, u, v)))
-#define rgba(u, v) (i16(inputRgb(3, u, v)))
+using namespace Halide;
+
 #define ycocgy(u, v) (i16(inputY(u, v)))
 #define ycocgco(u, v) ((i16(inputCo(u / 2, v / 2)) << 1) - 0xFF)
 #define ycocgcg(u, v) ((i16(inputCg(u / 2, v / 2)) << 1) - 0xFF)
-
-#define subsample(x) ((x(u * 2, v * 2) + x(u * 2 + 1, v * 2) + x(u * 2, v * 2 + 1) + x(u * 2 + 1, v * 2 + 1) + 1024) >> 3)
-#define subsample3(x) ((x(u * 2, v * 2, c) + x(u * 2 + 1, v * 2, c) + x(u * 2, v * 2 + 1, c) + x(u * 2 + 1, v * 2 + 1, c) + 1024) >> 3)
-
-#define input_value(x, t) Input<t> x { #x }
-
-#define input_buffer_u(x, b, d) Input<Func> x { #x, UInt(b), d }
-#define input_buffer_i(x, b, d) Input<Func> x { #x, Int(b), d }
-#define output_buffer_u(x, b, d) Output<Func> x { #x, UInt(b), d }
-#define output_buffer_i(x, b, d) Output<Func> x { #x, Int(b), d }
-
-using namespace Halide;
 
 class YCoCgR420ToRgb : public Generator<YCoCgR420ToRgb>
 {
@@ -36,12 +22,12 @@ public:
 	Func r { "r" };
 	Func g { "g" };
 	Func b { "b" };
-	
-	input_buffer_u(inputY, 8, 2);
-	input_buffer_u(inputCo, 8, 2);
-	input_buffer_u(inputCg, 8, 2);
-	
-	output_buffer_u(outputRgb, 8, 3);
+
+	Input<Func> inputY { "inputY", UInt(8), 2};
+	Input<Func> inputCo { "inputCo", UInt(8), 2};
+	Input<Func> inputCg { "inputCg", UInt(8), 2};
+
+	Output<Func> outputRgb { "outputRgb", UInt(8), 3 };
 	
 	void generate()
 	{
@@ -63,6 +49,12 @@ public:
 	}
 };
 
+#define rgbb(u, v) (i16(inputRgb(0, u, v)))
+#define rgbg(u, v) (i16(inputRgb(1, u, v)))
+#define rgbr(u, v) (i16(inputRgb(2, u, v)))
+
+#define subsample(x) ((x(u * 2, v * 2) + x(u * 2 + 1, v * 2) + x(u * 2, v * 2 + 1) + x(u * 2 + 1, v * 2 + 1) + 1024) >> 3)
+
 class RgbToYCoCgR420 : public Generator<RgbToYCoCgR420>
 {
 public:
@@ -76,12 +68,12 @@ public:
 	Func _co { "_co" };
 	Func _cg { "_cg" };
 	Func t { "t" };
-	
-	input_buffer_u(inputRgb, 8, 3);
-	
-	output_buffer_u(outputY, 8, 2);
-	output_buffer_u(outputCo, 8, 2);
-	output_buffer_u(outputCg, 8, 2);
+
+	Input<Func> inputRgb { "inputRgb", UInt(8), 3 };
+
+	Output<Func> outputY { "outputY", UInt(8), 2 };
+	Output<Func> outputCo { "outputCo", UInt(8), 2 };
+	Output<Func> outputCg { "outputCg", UInt(8), 2 };
 	
 	void generate()
 	{
@@ -110,14 +102,15 @@ public:
 
 	Func vert { "vert" };
 	Func horiz { "horiz" };
-	
-	input_value(width, int32_t);
-	input_value(height, int32_t);
-	input_buffer_u(inputRgbOld, 32, 2);
-	input_buffer_u(inputRgbNew, 32, 2);
-	
-	output_buffer_u(outputX, 32, 1);
-	output_buffer_u(outputY, 32, 1);
+
+	Input<int32_t> width { "width" };
+	Input<int32_t> height { "height" };
+
+	Input<Func> inputRgbOld { "inputRgbOld", UInt(32), 2 };
+	Input<Func> inputRgbNew { "inputRgbNew", UInt(32), 2 };
+
+	Output<Func> outputX { "outputX", UInt(32), 1 };
+	Output<Func> outputY { "outputY", UInt(32), 1 };
 	
 	void generate()
 	{
@@ -143,14 +136,15 @@ public:
 
 	Func vert { "vert" };
 	Func horiz { "horiz" };
-	
-	input_value(width, int8_t);
-	input_value(height, int8_t);
-	input_buffer_u(inputRgbOld, 8, 2);
-	input_buffer_u(inputRgbNew, 8, 2);
-	
-	output_buffer_u(outputX, 8, 1);
-	output_buffer_u(outputY, 8, 1);
+
+	Input<int8_t> width { "width" };
+	Input<int8_t> height { "height" };
+
+	Input<Func> inputRgbOld { "inputRgbOld", UInt(8), 2 };
+	Input<Func> inputRgbNew { "inputRgbNew", UInt(8), 2 };
+
+	Output<Func> outputX { "outputX", UInt(8), 1 };
+	Output<Func> outputY { "outputY", UInt(8), 1 };
 	
 	void generate()
 	{
@@ -174,8 +168,8 @@ public:
 	Var u { "u" };
 	Var v { "v" };
 
-	input_buffer_u(input, 32, 2);
-	output_buffer_u(output, 32, 2);
+	Input<Func> input { "input", UInt(32), 2 };
+	Output<Func> output { "output", UInt(32), 2 };
 
 	void generate()
 	{
@@ -188,6 +182,8 @@ public:
 	}
 };
 
+#define subsample3(x) ((x(u * 2, v * 2, c) + x(u * 2 + 1, v * 2, c) + x(u * 2, v * 2 + 1, c) + x(u * 2 + 1, v * 2 + 1, c) + 1024) >> 3)
+
 class Downscale2x : public Generator<Downscale2x>
 {
 public:
@@ -196,9 +192,9 @@ public:
 	Var c { "c" };
 
 	Func input16 { "input16" };
-	
-	input_buffer_u(input, 8, 3);
-	output_buffer_u(output, 8, 3);
+
+	Input<Func> input { "input", UInt(8), 3 };
+	Output<Func> output { "output", UInt(8), 3 };
 	
 	void generate()
 	{
@@ -233,11 +229,11 @@ public:
 	Func kY { "kY" };
 	Func resizedX { "resizedX" };
 	Func resizedY { "resizedY" };
-	
-	input_buffer_u(input, 8, 3);
-	input_value(scaleFactor, float);
-	
-	output_buffer_u(output, 8, 3);
+
+	Input<Func> input { "input", UInt(8), 3 };
+	Input<float> scaleFactor { "scaleFactor" };
+
+	Output<Func> output { "output", UInt(8), 3 };
 	
 	void generate()
 	{	
@@ -276,7 +272,7 @@ HALIDE_REGISTER_GENERATOR(Compare32Stage1, Compare32Stage1);
 HALIDE_REGISTER_GENERATOR(Downscale2x, Downscale2x);
 HALIDE_REGISTER_GENERATOR(Copy, Copy);
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 	return Halide::Internal::generate_filter_main(argc, argv, std::cerr);
 }
