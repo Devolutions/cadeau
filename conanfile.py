7de98d2c1b0ccce3f4f091c89c6279db7e0b0d31
Jean-François Théroux <jftheroux@devolutions.net>
2019-06-18 13:39:44 -0400

    migrating from conanfile.txt to conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conans import ConanFile
+import os
+
+class Importer(ConanFile):
+    name = 'importer'
+    settings = 'os', 'build_type'
+    generators = 'cmake'
+
+    def build_requirements(self):
+        if self.settings.os == 'Windows':
+            self.build_requires('clang-llvm/8.0.0-2@devolutions/stable')
+        else:
+            self.build_requires('clang-llvm/6.0.1-1@devolutions/stable')
+
+        def requirements(self):
+            self.requires('halide/1335f0219-2@devolutions/stable')
+            self.requires('libyuv/1661-3@devolutions/stable')
