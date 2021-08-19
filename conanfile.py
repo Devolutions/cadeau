ef0b18b155c5b80069ce68f4399836b87e375d89
Marc-André Moreau <marcandre.moreau@gmail.com>
2021-08-19 16:30:48 -0400

    ARM64 portability fixes (#9)
    Co-authored-by: Marc-André Moreau <mamoreau@devolutions.net>

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,5 @@ class Importer(ConanFile):
     generators = 'cmake'
 
     def build_requirements(self):
-        self.build_requires('clang-llvm/6.0.1-1@devolutions/stable')
-        self.build_requires('halide/1335f0219-2@devolutions/stable')
+        self.build_requires('halide/12.0.1-1@devolutions/stable')
         self.build_requires('libyuv/1661-3@devolutions/stable')
