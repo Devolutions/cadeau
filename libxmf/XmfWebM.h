97bc2fb2436172100d4a19c8dca461ac3748645b
irving ou <jou@devolutions.net>
2024-11-13 15:55:44 -0500

    clean up

--- libxmf/XmfWebM.h
+++ libxmf/XmfWebM.h
@@ -12,23 +12,22 @@ typedef struct xmf_webm XmfWebM;
 #include "XmfBipBuffer.h"
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-    XMF_EXPORT int XmfWebM_Encode(XmfWebM *ctx, const uint8_t *srcData, uint16_t x, uint16_t y, uint16_t width, uint16_t height);
-    XMF_EXPORT void XmfWebM_Finalize(XmfWebM *ctx);
+int XmfWebM_Encode(XmfWebM* ctx, const uint8_t* srcData, uint16_t x, uint16_t y, uint16_t width, uint16_t height);
+void XmfWebM_Finalize(XmfWebM* ctx);
 
-    XMF_EXPORT uint64_t XmfWebM_FrameCount(XmfWebM *ctx);
-    XMF_EXPORT uint64_t XmfWebM_Duration(XmfWebM *ctx);
+uint64_t XmfWebM_FrameCount(XmfWebM* ctx);
+uint64_t XmfWebM_Duration(XmfWebM* ctx);
 
-    XMF_EXPORT bool XmfWebM_Init(XmfWebM *ctx, uint32_t frameWidth, uint32_t frameHeight, uint32_t frameRate,
-                                 uint32_t targetBitRate, const char *filename, XmfBipBuffer *bb, XmfTimeSource *ts);
+bool XmfWebM_Init(XmfWebM* ctx, uint32_t frameWidth, uint32_t frameHeight, uint32_t frameRate,
+                uint32_t targetBitRate, const char* filename, XmfBipBuffer* bb, XmfTimeSource* ts);
 
-    XMF_EXPORT void XmfWebM_Uninit(XmfWebM *ctx);
+void XmfWebM_Uninit(XmfWebM* ctx);
 
-    XMF_EXPORT XmfWebM *XmfWebM_New();
-    XMF_EXPORT void XmfWebM_Free(XmfWebM *ctx);
+XmfWebM* XmfWebM_New();
+void XmfWebM_Free(XmfWebM* ctx);
 
 #ifdef __cplusplus
 }
