597d8c1e48efb0fe1e4fec2f623abc20aa66d5b5
Richard Markiewicz <rmarkiewicz@devolutions.net>
2025-03-03 12:44:57 -0500

    build: update zlib to 1.3.1

--- conanfile.py
+++ conanfile.py
@@ -9,5 +9,5 @@ class Importer(ConanFile):
     def requirements(self):
         self.requires('libjpeg/2.1.0@devolutions/stable')
         self.requires('libpng/1.6.39@devolutions/stable')
-        self.requires('zlib/1.2.11@devolutions/stable')
+        self.requires('zlib/1.3.1@devolutions/stable')
         self.requires('libvpx/1.10.0@devolutions/stable')
