03768df198a92db8e97ac69a6ced83403d7f31e8
irving ou <jou@devolutions.net>
2026-02-06 17:37:50 -0500

    feat(xmf-sys): expose XmfVpxEncoder_GetLastCreateError

--- rust/xmf-sys/src/lib.rs
+++ rust/xmf-sys/src/lib.rs
@@ -74,6 +74,7 @@ pub mod raw {
             fn XmfVpxEncoder_Flush(ctx: *mut XmfVpxEncoder) -> c_int,
             fn XmfVpxEncoder_Destroy(ctx: *mut XmfVpxEncoder) -> c_int,
             fn XmfVpxEncoder_GetLastError(ctx: *const XmfVpxEncoder) -> XmfVpxEncoderError,
+            fn XmfVpxEncoder_GetLastCreateError() -> XmfVpxEncoderError,
             fn XmfVpxEncoder_FreeEncodedFrame(input :*mut u8) -> (),
             fn XmfVpxEncoder_GetPacket(ctx: *mut XmfVpxEncoder, itr:*mut VpxIterator) -> *mut XmfVpxPacket,
 
